a4e1a970de1352f8f9acbc1a5f3e4fdd88ea2967
Yury Bayda <yury@bayda.pro>
2023-12-04 21:10:07 -0800

    Fix CI failure

--- telemetry_buffer.py
+++ telemetry_buffer.py
@@ -4,5 +4,5 @@ from bit_converter import BitConverter
 class TelemetryBuffer:
 
     @classmethod
-    def to_buffer(cls, reading: int) -> list[int]:
-        return [0x2] + BitConverter.get_bytes(reading) + [0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0]
+    def to_buffer(cls, reading):
+        return [0x2] + BitConverter.get_bytes(reading) + [0x0] * 7
